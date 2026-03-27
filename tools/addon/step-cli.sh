86907751fe149850559b50cba823cc2a19a6f0a1
Joerg Heinemann <heinemannj66@gmail.com>
2026-03-27 07:26:21 +0100

    Update step-cli.sh

--- tools/addon/step-cli.sh
+++ tools/addon/step-cli.sh
@@ -123,15 +123,15 @@ echo
 while true;
 do
 
-CA_FQDN=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step ca bootstrap options" --inputbox '\nCA FQDN (e.g. step-ca.example.com)' 10 50 "$CA_FQDN" 3>&1 1>&2 2>&3)
+CA_FQDN=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step-ca Bootstrap Options" --inputbox '\nCA FQDN (e.g. step-ca.example.com)' 10 50 "$CA_FQDN" 3>&1 1>&2 2>&3)
 IP=$(dig +short "$CA_FQDN")
 if [[ -z "$IP" ]]; then
     echo "Resolution failed for $CA_FQDN"
     exit
 fi
-FINGERPRINT=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step ca bootstrap options" --inputbox '\nCA Fingerprint' 10 50 "$FINGERPRINT" 3>&1 1>&2 2>&3)
+FINGERPRINT=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step-ca Bootstrap Options" --inputbox '\nCA Fingerprint' 10 50 "$FINGERPRINT" 3>&1 1>&2 2>&3)
 
-if whiptail_yesno=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step ca bootstrap options" --yesno "Continue with below?\n
+if whiptail_yesno=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "step-ca Bootstrap Options" --yesno "Continue with below?\n
 CA FQDN: $CA_FQDN
 CA Fingerprint: $FINGERPRINT" --no-button "Change" --yes-button "Continue" 15 70 3>&1 1>&2 2>&3); then
 break
