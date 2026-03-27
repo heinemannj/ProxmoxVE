383f1c401174dfd748e31c70b614809211fe7e7d
Joerg Heinemann <heinemannj66@gmail.com>
2026-03-27 10:36:26 +0100

    Update step-cli.sh

--- tools/addon/step-cli.sh
+++ tools/addon/step-cli.sh
@@ -221,9 +221,8 @@ function bootstrap() {
   step ca bootstrap -f --ca-url https://"$CA_FQDN" --install --fingerprint "$FINGERPRINT"  || die "CA Bootstrap failed!"
   step certificate install --all ~/.step/certs/root_ca.crt || die "Installing root CA certificate failed!"
   update-ca-certificates  || die "Update of System CA Certificates failed!"
-  
+  echo -e "\nTest"
   $STD step certificate inspect https://"$CA_FQDN" || die "Main - Certificate Inspect failed!"
-  
   msg_ok "Installed root CA certificate"
 }
 
