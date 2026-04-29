b29d6cda3fef02268e147c584ad6e0325487bf6e
Joerg Heinemann <heinemannj66@gmail.com>
2026-04-29 12:42:46 +0200

    Uncomment step-ca commands in installation script

--- install/step-ca-install.sh
+++ install/step-ca-install.sh
@@ -320,7 +320,7 @@ ReadOnlyPaths=/etc/step-ca
 [Install]
 WantedBy=multi-user.target
 EOF
-#$STD systemctl enable -q --now step-ca
+$STD systemctl enable -q --now step-ca
 msg_ok "Started step-ca as a Daemon"
 
 msg_info "Configuring step-ca Admins and Provisioners"
@@ -331,43 +331,43 @@ AdminCert="$AdminDir/admin.crt"
 AdminKey="$AdminDir/admin.key"
 mkdir -p "$AdminDir"
 
-#$STD step ca certificate step \
-#  "$AdminCert" \
-#  "$AdminKey" \
-#  --provisioner="Admin JWK" \
-#  --provisioner-password-file=$ProvisionerPwdFile
+$STD step ca certificate step \
+  "$AdminCert" \
+  "$AdminKey" \
+  --provisioner="Admin JWK" \
+  --provisioner-password-file=$ProvisionerPwdFile
 
-#$STD step ca provisioner add "$PKIProvisioner" \
-#  --type JWK \
-#  --admin-name="$PKIProvisioner" \
-#  --admin-cert="$AdminCert" \
-#  --admin-key="$AdminKey"
+$STD step ca provisioner add "$PKIProvisioner" \
+  --type JWK \
+  --admin-name="$PKIProvisioner" \
+  --admin-cert="$AdminCert" \
+  --admin-key="$AdminKey"
 
-#$STD step ca provisioner add "$AcmeProvisioner" \
-#  --type ACME \
-#  --admin-name "$AcmeProvisioner" \
-#  --admin-cert="$AdminCert" \
-#  --admin-key="$AdminKey"
+$STD step ca provisioner add "$AcmeProvisioner" \
+  --type ACME \
+  --admin-name "$AcmeProvisioner" \
+  --admin-cert="$AdminCert" \
+  --admin-key="$AdminKey"
 
-#$STD step ca provisioner update "$PKIProvisioner" \
-#  --x509-min-dur="$X509MinDur" \
-#  --x509-max-dur="$X509MaxDur" \
-#  --x509-default-dur="$X509DefaultDur" \
-#  --x509-template="$X509LeafTemplate" \
-#  --x509-template-data="$X509LeafTemplateData" \
-#  --allow-renewal-after-expiry \
-#  --admin-cert="$AdminCert" \
-#  --admin-key="$AdminKey"
+$STD step ca provisioner update "$PKIProvisioner" \
+  --x509-min-dur="$X509MinDur" \
+  --x509-max-dur="$X509MaxDur" \
+  --x509-default-dur="$X509DefaultDur" \
+  --x509-template="$X509LeafTemplate" \
+  --x509-template-data="$X509LeafTemplateData" \
+  --allow-renewal-after-expiry \
+  --admin-cert="$AdminCert" \
+  --admin-key="$AdminKey"
 
-#$STD step ca provisioner update "$AcmeProvisioner" \
-#  --x509-min-dur="$X509MinDur" \
-#  --x509-max-dur="$X509MaxDur" \
-#  --x509-default-dur="$X509DefaultDur" \
-#  --x509-template="$X509LeafTemplate" \
-#  --x509-template-data="$X509LeafTemplateData" \
-#  --allow-renewal-after-expiry \
-#  --admin-cert="$AdminCert" \
-#  --admin-key="$AdminKey"
+$STD step ca provisioner update "$AcmeProvisioner" \
+  --x509-min-dur="$X509MinDur" \
+  --x509-max-dur="$X509MaxDur" \
+  --x509-default-dur="$X509DefaultDur" \
+  --x509-template="$X509LeafTemplate" \
+  --x509-template-data="$X509LeafTemplateData" \
+  --allow-renewal-after-expiry \
+  --admin-cert="$AdminCert" \
+  --admin-key="$AdminKey"
 
 chown -R step:step "$(step path)"
 chmod -R 700 "$(step path)"
